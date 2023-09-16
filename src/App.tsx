9fa6d45d9c9d07b995f21dada82da9c2df0b28e2
UnrealOG <creepercraft137@gmail.com>
2023-09-15 20:25:04 -0400

    First commit
    Has some css and buttons, but no real functionality yet

---
+++ src/App.tsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/tauri";
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+import {Route, Link, Routes} from 'react-router-dom';
+import { useNavigate } from 'react-router'
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = useState("");
+  const [name, setName] = useState("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name }));
+  }
+
+  const navigate=useNavigate()
+
+  return (
+    //container holds the entire thing
+    <div className="container">
+
+      <div className="sidebar">
+        <form
+          className="row"
+          onSubmit={(e) => {
+            e.preventDefault();
+            greet();
+          }}
+        >
+          <input
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a name..."
+          />
+          <button type="submit">Greet</button>
+        </form>
+        <button>Distance</button>
+        <button>Volume</button>
+        <button>Area</button>
+        <button>Temperature</button>
+      </div>
+      
+      <div className="applet">
+      <p>{greetMsg}</p>
+
+        <h1>Unit Converter</h1>
+        <Link to='/home' onClick={() => navigate('/home')}>home link</Link>
+        <Link to='/home1' onClick={() => navigate(1)}>Bad</Link>
+        <Link to='/' onClick={() => navigate(1)}>main</Link>
+        <p>Hi</p>
+        <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<br />} />
+        <Route path='*' element={<NotFound />} />
+        </Routes>
+      </div>
+      {/* The forbidden zone */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import '../App.css';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Page not found</h1>
+        </div>
+    )
+}
+
+export default NotFound
