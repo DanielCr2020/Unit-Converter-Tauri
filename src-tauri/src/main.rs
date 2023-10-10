a0aa2b87bba0511c7aee6e2d6c3767b38a37a9e9
UnrealOG <creepercraft137@gmail.com>
2023-10-10 00:56:47 -0400

    Now using SolidJS instead of React
    Yeeted Typescript into the shadow realm

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,18 +8,34 @@
 // }
 
 fn convert_distance(number: f64, from: &str, to: &str) -> f64 {
-    let mut multiplier: f64 = match from {
+    let mut multiplier: f64 = match from {      //x to feet
         "feet" => 1.0,  //base unit (probably)
         "inches" => 1.0/12.0,
         "miles" => 5280.0,
         "meters" => 12.0/3.375,
         _ => 0.0
     };
-    multiplier *= match to {
+    multiplier *= match to {            //feet to y
         "feet" => 1.0,
         "inches" => 12.0,
         "miles" => 1.0/5280.0,
         "meters" => 3.375/12.0,
+        _ => 0.0
+    };
+    return number*multiplier;
+}
+
+fn convert_volume(number: f64, from: &str, to: &str) -> f64 {
+    let mut multiplier: f64 = match from {      //x to liters
+        "liters" => 1.0,     //base unit
+        "gallons" => 3.785412,
+        "quarts" => 0.946353,  
+        _ => 0.0
+    };
+    multiplier *= match to {        //liters to y
+        "liters" => 1.0,
+        "gallons" => 1.0/3.785412,
+        "quarts" => 1.0/0.946353,  
         _ => 0.0
     };
     return number*multiplier;
@@ -30,6 +46,9 @@ fn convert(number: f64, convert_from: &str, convert_to: &str, convert_unit: &str
     //convert every input to some base unit. Then convert that to the output unit
     if convert_unit=="Distance" {
         return convert_distance(number, convert_from, convert_to)
+    }
+    else if convert_unit=="Volume" {
+        return convert_volume(number, convert_from, convert_to)
     }
 
     return 0.0;
